83783a855aad30f406bbb5023fbf7241bcad3553
Larissa Varjão <larissasilvavarjao@gmail.com>
2020-06-17 21:21:31 -0300

    feat: draft migrations to syntoms

--- data/migrations/20200409181449_users.js
+++ data/migrations/20200409181449_users.js
@@ -1,21 +1,19 @@
 exports.up = function (knex) {
-  return knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('users', (table) => {
-      table.uuid('id').unique().notNullable().defaultTo(knex.raw('uuid_generate_v4()'));
-      table.text('first_name', 128).notNullable();
-      table.text('last_name', 128);
-      table.text('pacient_name', 128);
-      table.text('email', 128).notNullable().unique();
-      table.text('password', 128);
-      table.text('google_id');
-      table.text('facebook_id');
-      table.datetime('created_at', { precision: 6 }).notNullable().defaultTo(knex.fn.now());
-      table.datetime('updated_at', { precision: 6 }).notNullable().defaultTo(knex.fn.now());
-      table.datetime('deleted_at', { precision: 6 });
-    });
+  return knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"').createTable("users", (table) => {
+    table.uuid("id").unique().notNullable().defaultTo(knex.raw("uuid_generate_v4()"));
+    table.text("first_name", 128).notNullable();
+    table.text("last_name", 128);
+    table.text("pacient_name", 128);
+    table.text("email", 128).notNullable().unique();
+    table.text("password", 128);
+    table.text("google_id");
+    table.text("facebook_id");
+    table.datetime("created_at", { precision: 6 }).notNullable().defaultTo(knex.fn.now());
+    table.datetime("updated_at", { precision: 6 }).notNullable().defaultTo(knex.fn.now());
+    table.datetime("deleted_at", { precision: 6 });
+  });
 };
 
 exports.down = function (knex) {
-  knex.schema.dropTableIfExists('users');
+  knex.schema.dropTableIfExists("users");
 };
---
+++ data/migrations/20200617211553_syntoms_users.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("syntoms_users", (table) => {
+    table.uuid("id").unique().notNullable().defaultTo(knex.raw("uuid_generate_v4()"));
+    table.text("name").notNullable();
+    table.text("description");
+    table.text("classification");
+    table.uuid("user_id").notNullable().references("id").inTable("users");
+    table.datetime("created_at", { precision: 6 }).notNullable().defaultTo(knex.fn.now());
+    table.datetime("updated_at", { precision: 6 }).notNullable().defaultTo(knex.fn.now());
+    table.datetime("deleted_at", { precision: 6 });
+  });
+};
+
+exports.down = function (knex) {
+  knex.schema.dropTableIfExists("syntoms_users");
+};
